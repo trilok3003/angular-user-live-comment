5d3bf731ad71c85b1045cd7c4f3323a466b45afd
Trilok Singh <trilok.singh@BIOREV.STUDIO>
2019-05-29 12:32:41 +0530

    first commit

---
+++ src/app/pusher.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+declare const Pusher: any;
+
+@Injectable()
+export class PusherService {
+  constructor() {
+  // Replace this with your pusher key    
+    this.pusher = new Pusher('PUSHER_KEY', {
+      cluster: 'eu',
+      encrypted: true,
+    });
+  }
+  pusher;
+
+  public init(channel) {
+    return this.pusher.subscribe(channel);
+  }
+}
